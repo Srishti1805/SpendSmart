a251c0f60d71cb6dbdbb4cc6929b5e501249fd43
Srishti1805 <72729278+Srishti1805@users.noreply.github.com>
2024-04-26 21:54:09 +0530

    add category script.

--- CreateDb/generateData.js
+++ CreateDb/generateData.js
@@ -3,17 +3,66 @@ db = db.getSiblingDB('spendSmart')
 // Create a collection for users
 db.createCollection('users')
 // users collection
-listsCollection = db.getCollection("users")
+usersCollection = db.getCollection("users")
 // remove existing data
-listsCollection.remove({})
+usersCollection.remove({})
 
 // create new data for users
 // Insert 10 users into the users collection
 for (var i = 0; i < 10; i++) {
-    db.users.insert({
+    usersCollection.insert({
         fname: "First" + (i + 1),
         lname: "Last" + (i + 1),
         email: "user" + (i + 1) + "@example.com",
         password: "password" + (i + 1)
     });
 }
+
+// Create a collection for category
+db.createCollection('category')
+// category collection
+categoryCollection = db.getCollection("category")
+// remove existing data
+categoryCollection.remove({})
+
+// create new data for category
+// Insert 5 users into the users collection
+categoryCollection.insert(
+{
+    userId: 'user1',
+    name: 'category 1',
+    description: 'description 1'
+}
+)
+
+categoryCollection.insert(
+{
+    userId: 'user1',
+    name: 'category 2',
+    description: 'description 2'
+}
+)
+
+categoryCollection.insert(
+{
+    userId: 'user1',
+    name: 'category 3',
+    description: 'description 3'
+}
+)
+
+categoryCollection.insert(
+{
+    userId: 'user1',
+    name: 'category 4',
+    description: 'description 4'
+}
+)
+
+categoryCollection.insert(
+{
+    userId: 'user1',
+    name: 'category 5',
+    description: 'description 5'
+}
+)
